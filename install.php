a58480b84b2742d93affb57bccceebb764a66984
Youssef Touama <touamayoussef@gmail.com>
2019-12-12 14:56:46 +0100

    styler

--- install.php
+++ install.php
@@ -1,17 +1,22 @@
 <!DOCTYPE html> 
 <html> 
 <head> 
-    <title> 
-        Install the db
-    </title> 
+    <title>inisialisation de la base de donnees</title> 
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="style/style.css">
 </head> 
   
 <body style="text-align:center;"> 
-      
-    <h1 style="color:green;"> 
-        initialiser la base de donnes 
-    </h1> 
-  
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+            <a class="nav-item nav-link active" href="#">Initialiser la base de donnees<span class="sr-only">(current)</span></a>
+            </div>
+        </div>
+    </nav>
     <?php
         error_reporting(E_ERROR | E_WARNING | E_PARSE);
         if(isset($_POST['button'])) {
@@ -54,13 +59,26 @@ if (\$conn->connect_error) {
         
         
     ?> 
-      
+    <div class="container" style="margin-top:150px">
     <form method="POST"> 
-        <input type="text" name="username" placeholder="db username"/>
-        <input type="password" name="password" placeholder="db password"/>
-        <input type="text" name="dbname" placeholder="db name"/><br>
-        <input type="submit" name="button" value="Creer la base de donner"/> 
+         <div class="input-group mb-6 a">
+            <div class="input-group-prepend">
+                <span class="input-group-text b" id="basic-addon1">le nom d'utilisateur</span><input class="form-control" type="text" name="username" placeholder="db username"/>
+            </div>
+        </div>
+         <div class="input-group mb-6 a">
+            <div class="input-group-prepend">
+                <span class="input-group-text b" id="basic-addon1">mot de pass</span><input class="form-control" type="password" name="password" placeholder="db password"/>
+            </div>
+        </div>
+         <div class="input-group mb-6 a">
+            <div class="input-group-prepend">
+                <span class="input-group-text b" id="basic-addon1">nom de la bd</span><input class="form-control" type="text" name="dbname" placeholder="db name"/>
+            </div>
+        </div>
+        <button class="btn btn-primary" type="submit" name="button">Creer la base de donnees</button> 
     </form> 
-</head> 
+    </div>
+</body> 
   
 </html> 
